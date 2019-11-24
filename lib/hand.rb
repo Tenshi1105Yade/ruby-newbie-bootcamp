742ab00efc8e58740faa52f80feda4e5723ba502
Tenshi1105Yade <banana115yaro@gmail.com>
2019-11-24 06:40:43 +0000

    5つできた

--- lib/hand.rb
+++ lib/hand.rb
@@ -4,15 +4,51 @@ require_relative 'card.rb'
 # TODO: Basically, extend this class (you can create another class, of course!)
 class Hand
   attr_reader :cards, :rank
-
+   
   def initialize(draw_cards)
     @cards = draw_cards.map { |str| Card.new(str) }
     @rank = categorize
   end
 
   private
-
+  
+ 
   def categorize
-    'high-card'
+      cnt1 = 0
+      cnt2 = 0
+    @cards.each do |card|
+      if card.suit == @cards[0].suit
+        cnt1=cnt1+1
+      end
+      if card.face == @cards[0].face
+        cnt2=cnt2+1
+      end
+
+    end
+
+    if cnt1==5
+      'flush'
+    elsif cnt2==4
+      'four-of-a-kind'
+    elsif cnt2==3
+      'three-of-a-kind'
+    elsif cnt2==2
+      'one-pair'
+    else
+      'high-card'
+    end
+
+     
+     
+     
+     
+     
+     
+     
+     
+     
+  
+
   end
+    
 end
